f747ada59a6d5c6deefaed6275190a6cc5522c6e
pardhasm <pardhasaradhi108@gmail.com>
2021-04-11 22:36:25 +0530

    exploring yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,56 @@
-const getNotes = require('./notes')
 const chalk = require('chalk')
-const fs = require('fs')
+const yargs = require('yargs')
+const getNotes = require('./notes')
 
-const msg = getNotes()
+yargs.version("1.1.0")
 
-console.log(chalk.green.bold("Success"))
-fs.appendFileSync('chalk.txt',chalk.green.bold("Success"))
+//Add command
+yargs.command({
+    command: "add",
+    description: "Add a new note",
+    builder: {
+        title: {
+            describe: "Note title",
+            demandOption: true,
+            type: "string"
+        },
+        body: {
+            describe: "Note body",
+            demandOption: true,
+            type: "string"
+        }
+    },
+    handler: function (argv) {
+        console.log("Title : ", argv.title, " Body : ", argv.body)
+    }
+})
+
+//Remove command
+yargs.command({
+    command: "remove",
+    description: "Remove the note",
+    handler: function () {
+        console.log("Removing the note")
+    }
+})
+
+//List command
+yargs.command({
+    command: "list",
+    description: "List all notes",
+    handler: function () {
+        console.log("Listing out all notes")
+    }
+})
+
+//Read command
+yargs.command({
+    command: "read",
+    description: "Read a note",
+    handler: function () {
+        console.log("Reading a note")
+    }
+})
+
+yargs.parse()
+
